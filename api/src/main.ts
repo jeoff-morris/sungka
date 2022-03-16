a5e0cf74abc243ae011f952c3f343df119c51d5a
Jeoff Morris <jeoffmorris@icloud.com>
2022-03-16 08:24:01 +0000

    added user registration, passport, password hashing and sessions

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 import * as session from 'express-session';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.use(cookieParser());
 	app.use(
 		session({
 			secret: process.env.SESSION_SECRET,
